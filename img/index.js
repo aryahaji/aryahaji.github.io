9d6c5005d8f64b595e7f6e1b113f98e93245e2ab
Arya Hajikandi <aryahaji@buffalo.edu>
2018-05-01 19:03:05 -0400

    Add files via upload

---
+++ img/index.js
@@ -0,0 +1,54 @@
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 240
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true // Choose whether you can drag to open on touch screens
+
+});
+
+
+// Dropdown JS
+$('.dropdown-button').dropdown({
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: false, // Does not change width of dropdown to that of the activator
+  hover: true, // Activate on hover
+  gutter: 0, // Spacing from edge
+  belowOrigin: false, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+});
+
+// JS initialization for Parallax
+$(document).ready(function() {
+  $('.parallax').parallax();
+});
+
+// Carousel initialization
+$(document).ready(function() {
+  $('.carousel').carousel();
+});
+
+
+//Smooth Scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  $('.tooltipped').tooltip({
+    delay: 50
+  });
+});
